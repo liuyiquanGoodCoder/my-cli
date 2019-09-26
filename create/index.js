a4885119f7c6cfeeb04437761f388daac64f61ee
刘艺全(实习) <liuyiquan@megvii.com>
2019-09-26 17:19:27 +0800

    finish generator

--- create/index.js
+++ create/index.js
@@ -5,10 +5,17 @@ const fs = require("fs");
 module.exports = function (name) {
 	mkdirp(path.join(process.cwd(),name),function (err) {
 		if (err) {
-			console.error("created fail")
+			console.error("created failed")
 		} else {
 			console.log(`created ${name} folder successful`);
 			const {tempalte, dir, name:filenName} = require("../generator/package")(name);
+			fs.writeFile(path.join(projectDir,dir,filenName),tempalte.trim(),function (err) {
+				if (err) {
+					console.log(`created ${filenName} file failed`)
+				} else {
+					console.log(`created ${filenName} file successful`)
+				}
+			})
 		}
 	});
 }
