a0fe019e1e3dc0ee68eca5afbde976661d299c38
刘艺全(实习) <liuyiquan@megvii.com>
2019-09-26 19:32:20 +0800

    finish the function

--- create/index.js
+++ create/index.js
@@ -3,13 +3,14 @@ const mkdirp = require("mkdirp");
 const fs = require("fs");
 
 module.exports = function (name) {
-	mkdirp(path.join(process.cwd(),name),function (err) {
+	const projectDir = path.join(process.cwd(),name);
+	mkdirp(projectDir,function (err) {
 		if (err) {
 			console.error("created failed")
 		} else {
 			console.log(`created ${name} folder successful`);
-			const {tempalte, dir, name:filenName} = require("../generator/package")(name);
-			fs.writeFile(path.join(projectDir,dir,filenName),tempalte.trim(),function (err) {
+			const {template, dir, name:filenName} = require("../generator/index-html")(name);
+			fs.writeFile(path.join(projectDir,dir,filenName),template.trim(),function (err) {
 				if (err) {
 					console.log(`created ${filenName} file failed`)
 				} else {
